59014270cf76b4c0c46e93efcd0418f9f680e042
Mahitha <Mahitha@Mahitha-PC.fios-router.home>
2017-11-01 17:57:16 -0400

    initial Lambdas

--- Java8/src/com/lambdas/Lambdas.java
+++ Java8/src/com/lambdas/Lambdas.java
@@ -1,9 +1,26 @@
 package com.lambdas;
 
-public class Lambdas {
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
 
-	public static void main(String[] args) {
+public class Lambdas 
+{
+
+	private static boolean isPrime(int number) {
+		IntPredicate isDivisible = index -> number % index == 0;
+		
+		return number > 1
+				&& IntStream.range(2, number).noneMatch(
+						isDivisible);
+	}
+	
+	public static void main(String[] args) 
+	{
 		System.out.println("Hello World");
+		System.out.println(isPrime(7));
+		// Introducing Lambdas...
+		
+		
 
 	}
 
